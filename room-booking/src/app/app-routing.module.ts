b24051ae99f1c93e0e77387d1207606d2e8835d5
Antara1410 <antarapandey2004@gmail.com>
2023-03-03 04:43:05 +0530

    login and booking draft 2

--- room-booking/src/app/app-routing.module.ts
+++ room-booking/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookingComponent } from './booking/booking.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'booking', component:BookingComponent},
+  {path:'login', component:LoginComponent}
+]
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
