2810dca1788b235017063e660a8714279d531914
Antara1410 <antarapandey2004@gmail.com>
2023-03-03 03:07:42 +0530

    login and booking first draft

--- room-booking/src/app/app.module.ts
+++ room-booking/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageCardsComponent } from './home-page-cards/home-page-cards.component';
+import { LoginComponent } from './login/login.component';
+import { BookingComponent } from './booking/booking.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageCardsComponent
+    HomePageCardsComponent,
+    LoginComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
