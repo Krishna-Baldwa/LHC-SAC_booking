0e60ae61145a1e3211ffe046d27b12f5f6d8dd4b
Shivam <shivamkr00137400@gmail.com>
2023-03-03 16:55:15 +0530

    Final changes done

--- src/app/pending-req/pending-req.component.ts
+++ src/app/pending-req/pending-req.component.ts
@@ -8,9 +8,25 @@ import { RequestsdataService } from '../services/requestsdata.service';
 })
 export class PendingReqComponent {
   requests:any;
+  viewstatus:boolean | undefined;
   constructor(private requestdata: RequestsdataService)
   {
     console.warn("requestsdata",requestdata.getRequestsData())
     this.requests=requestdata.getRequestsData();
+    /*this.requestdata.getRequestsData().subscribe((data)=>
+      {this.requests=data;});*/ //For getting api data from service.
+  }
+  Isaccepted(status:boolean){//When accepted this parameter of status will be true and false if admin rejects.
+      this.viewstatus=false;
+      this.viewstatus=!this.viewstatus;//Either of accepted or rejected when click will change viewstatus to true.
+      this.submit_status(status,this.viewstatus);
+  }
+  submit_status(data1:boolean,data2:boolean){
+    console.warn(data1);
+    console.warn(data2);
+    //To send this status data to service to send it back via post api.
+    /*this.requestdata.sendstatus(data1,data2).subscribe((result)=>{
+      console.warn(result);
+    });*/ 
   }
 }
