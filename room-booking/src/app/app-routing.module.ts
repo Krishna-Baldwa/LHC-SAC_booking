4686d7dbab612ea74425734267683b578d7bba45
Antara1410 <antarapandey2004@gmail.com>
2023-03-03 20:18:04 +0530

    final draft but missing past bookings

--- room-booking/src/app/app-routing.module.ts
+++ room-booking/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookingComponent } from './booking/booking.component';
+import { HomePageCardsComponent } from './home-page-cards/home-page-cards.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  {path:'', component:HomePageCardsComponent},
   {path:'booking', component:BookingComponent},
   {path:'login', component:LoginComponent}
 ]
--- room-booking/src/app/home-page-cards/home-page-cards.component.ts
+++ room-booking/src/app/home-page-cards/home-page-cards.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page-cards.component.css']
 })
 export class HomePageCardsComponent {
-   image_sources:string[] = ["./assets/lh.jpeg", "./assets/la.jfif", "./assets/lc.jpg", "./assets/lt.jpg"];
+  image_sources:string[][] = [["./assets/lh.jpeg", "LH-240"], ["./assets/la.jfif", "LA-350"], ["./assets/lc.jpg", "LC-150"], ["./assets/lt.jpg", "LT-60"]];
+  //  image_sources:string[] = ["./assets/lh.jpeg", "./assets/la.jfif", "./assets/lc.jpg", "./assets/lt.jpg"];
 }
